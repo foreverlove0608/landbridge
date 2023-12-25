26b1882b76eb72a9065437dd89e42c95cf9f06aa
Hung <hung@m.ibis.studio>
2023-12-25 15:53:16 +0700

    up

--- src/js/setting.js
+++ src/js/setting.js
@@ -15,3 +15,21 @@ jQuery(function ($) {
 		$('.results-top__list-ttl').matchHeight();
 	});
 });
+
+$(function () {
+	var urlHash = location.hash;
+	if (urlHash) {
+		$('body,html').stop().scrollTop(0);
+		setTimeout(function () {
+			var target = $(urlHash);
+			var position = target.offset().top;
+			$('body,html').stop().animate({scrollTop: position}, 1000);
+		}, 100);
+	}
+
+	jQuery('.js-scroll').click(function(e) {
+		jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top }, 1500);
+		return false;
+		e.preventDefault();
+	});
+});
